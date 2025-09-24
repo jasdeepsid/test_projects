3a0898f28b66cd6717284602bff5c280dbf5f2b2
jasdeepsid <jasdeepsidhu647@gmail.com>
2025-09-23 18:16:53 -0600

    comment

--- src/tests/apiData.test.js
+++ src/tests/apiData.test.js
@@ -1,32 +1,33 @@
 //import the function to be tested
-const fetch = require('./functions/apiData.js');
+const { fetchData } = require('../functions/apiData.js');
 
 //Mock this global fetch function
-global.fetch = jest.fn(); 
+global.fetch = jest.fn();
 
 //test suite
-describe('fetchData',() => {
+describe('fetchData',  () => {
     beforeEach(() => {
-        //clear the object before running the test again
+        //clear the object before running the test again.  
         fetch.mockClear();
     });
 
-    test('fetches data from API endpoint and returns it as JSON', async () =>{
+    test('fetches data from the API endpoint and returns it as JSON', async () =>{
 
-        //AAA pattern
-        //Arrange
-        //Mock response
-        const mockRwsponse = {userId: 1, id: 1, title: 'delectus aut autem', completed: false}
+        /*AAA pattern
+          Arrange
+          Mock response*/
+        const mockResponse = {userId: 1, id: 1, title: 'delectus aut autem', completed: false}
         fetch.mockResolvedValueOnce({
             ok: true,
             json: jest.fn().mockResolvedValueOnce(mockResponse)
+        });
+
+        //Action
+        const data = await fetchData('https://jsonplaceholder.typicode.com/todos/1');
+
+        //Assert
+        expect(data).toEqual(mockResponse);
+        expect(fetch).toHaveBeenCalledWith('https://jsonplaceholder.typicode.com/todos/1');
     });
 
-    //Action
-    const data =  await fetch('https://jsonplaceholder.typicode.com/todos/1');
-
-
-    
-    
-
-})
+});
